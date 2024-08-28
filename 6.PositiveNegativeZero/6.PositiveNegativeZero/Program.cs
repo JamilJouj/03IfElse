475b9eacd0d999f388c0f664307bd9da5225cfc3
Jones <jamil.jouj@iths.se>
2024-08-29 00:59:04 +0200

    PositiveNegativeZero

---
+++ 6.PositiveNegativeZero/6.PositiveNegativeZero/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number:");
+        double number = Convert.ToDouble(Console.ReadLine());
+
+        if (number > 0)
+        {
+            Console.WriteLine("The number is positive");
+        }
+        else if (number < 0)
+        {
+            Console.WriteLine("The number is negative");
+        }
+        else
+        {
+            Console.WriteLine("The number is zero");
+        }
+    }
+}
