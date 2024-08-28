0f5db56c075b66061c032a8b77872fcd18972ecc
Jones <jamil.jouj@iths.se>
2024-08-29 00:51:59 +0200

    MAximumOfThree

---
+++ 5.MaximumOfThree/5.MaximumOfThree/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the first number:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter the third number:");
+        double num3 = Convert.ToDouble(Console.ReadLine());
+
+        double max;
+
+        if (num1 >= num2 && num1 >= num3)
+        {
+            max = num1;
+        }
+        else if (num2 >= num1 && num2 >= num3)
+        {
+            max = num2;
+        }
+        else
+        {
+            max = num3;
+        }
+
+        Console.WriteLine("The maximum number is: " + max);
+    }
+}
