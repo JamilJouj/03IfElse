9327c5a58d8e48647e89d9d6fcea513a72c0c76f
Jones <jamil.jouj@iths.se>
2024-08-28 15:00:55 +0200

    AgeBracket

--- 2.AgeBracket/2.AgeBracket/Program.cs
+++ 2.AgeBracket/2.AgeBracket/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Runtime.InteropServices;
+{
+    int age;
+    Console.WriteLine("Input ur age");
+
+    age = Convert.ToInt32(Console.ReadLine());
+
+
+    if (age < 13)
+    {
+        Console.WriteLine("you are a child");
+    }
+    else if (age >= 13 && age <= 19)
+    {
+        Console.WriteLine("you are a teenager");
+    }
+    else
+    {
+        Console.WriteLine("You are an adult");
+
+    }
+}
