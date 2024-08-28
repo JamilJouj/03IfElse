0c0f01b9a9839d8481eb6fb583beb157fb1185e0
Jones <jamil.jouj@iths.se>
2024-08-29 01:05:14 +0200

    DiscountSystem

---
+++ 7.DiscountSystem/7.DiscountSystem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the total amount of the purchase:");
+        double totalAmount = Convert.ToDouble(Console.ReadLine());
+
+        double finalAmount = totalAmount;
+
+        if (totalAmount > 1000)
+        {
+            double discount = totalAmount * 0.10;
+            finalAmount = totalAmount - discount;
+        }
+
+        Console.WriteLine($"The final amount after discount is: ${finalAmount:F2}");
+    }
+}
