128edafcc6a240755b17393a26a2d455aeb6e599
Jones <jamil.jouj@iths.se>
2024-08-28 14:25:14 +0200

    Add project files.

---
+++ 1.OddorEven/1.OddorEven/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+int number;
+
+Console.WriteLine("Input a number: ");
+
+number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("The number is even.");
+    return;
+}
+
+Console.WriteLine("The number is odd.");
