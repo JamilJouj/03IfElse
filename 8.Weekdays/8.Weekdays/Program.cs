3bde2d1d4ea03baf224ad701c020a8bb93b0d47d
Jones <jamil.jouj@iths.se>
2024-08-29 01:07:52 +0200

    Weekdays

---
+++ 8.Weekdays/8.Weekdays/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number between 1 and 7:");
+        int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+        string dayOfWeek;
+
+        switch (dayNumber)
+        {
+            case 1:
+                dayOfWeek = "Monday";
+                break;
+            case 2:
+                dayOfWeek = "Tuesday";
+                break;
+            case 3:
+                dayOfWeek = "Wednesday";
+                break;
+            case 4:
+                dayOfWeek = "Thursday";
+                break;
+            case 5:
+                dayOfWeek = "Friday";
+                break;
+            case 6:
+                dayOfWeek = "Saturday";
+                break;
+            case 7:
+                dayOfWeek = "Sunday";
+                break;
+            default:
+                dayOfWeek = "Error: Please enter a number between 1 and 7.";
+                break;
+        }
+
+        Console.WriteLine(dayOfWeek);
+    }
+}
